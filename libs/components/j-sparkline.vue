d4d1dc345a37922bf4ebce029d317c75297a2f22
Huỳnh Quang Phúc <quangphuc.huynh95@gmail.com>
2021-07-01 22:52:43 +0700

    add sparkline

--- libs/components/j-sparkline.vue
+++ libs/components/j-sparkline.vue
@@ -1,11 +1,84 @@
 <template>
-$END$
+  <svg
+    ref="svgRef"
+    xmlns="http://www.w3.org/2000/svg"
+    aria-labelledby="title"
+    :width="width || '100%'"
+    :height="height || '100%'"
+    role="img"
+  >
+    <g>
+      <polyline
+        fill="none"
+        :stroke="color"
+        :strokeWidth="strokeWidth"
+        :points="points?.map((xy) => xy.join(',')).join(' ')"
+      />
+    </g>
+  </svg>
 </template>
 
-<script>
-export default {
-name: "j-sparkline"
-}
+<script lang="ts">
+import { computed, defineComponent, PropType, ref } from 'vue';
+
+export default defineComponent({
+  name: 'JSparkline',
+  props: {
+    data: {
+      type: Array as PropType<(string|number)[]>,
+      required: true,
+    },
+    color: {
+      type: String,
+      default: 'blue',
+    },
+    strokeWidth: {
+      type: [String, Number],
+      default: 1,
+    },
+    width: {
+      type: [String, Number],
+      default: null,
+    },
+    height: {
+      type: [String, Number],
+      default: null,
+    },
+  },
+  setup(props) {
+    const svgRef = ref<SVGElement|null>(null);
+    const size = computed(() => {
+      return {
+        width: Number(props.width || svgRef.value?.getBoundingClientRect().width),
+        height: Number(props.height || svgRef.value?.getBoundingClientRect().height),
+      }
+    })
+
+    const points = computed<[number, number][]>(() => {
+      if (!size.value.width || !size.value.height) {
+        return [];
+      }
+      let min = props.data.map(Number).reduce<number>((m, item) => (item < m ? item : m), Number.POSITIVE_INFINITY);
+      let max = props.data.map(Number).reduce<number>((m, item) => (item > m ? item : m), Number.NEGATIVE_INFINITY);
+      let range = max - min;
+
+      if (range === 0) {
+        range = max;
+      }
+
+      min -= (range * 0.1);
+      max += (range * 0.1);
+      range = max - min;
+      const width = size.value.width / (props.data.length - 1);
+
+      return props.data.map(Number).map((item, index) => [index * width, size.value.height - (((item - min) * size.value.height) / range)]);
+    })
+    return {
+      svgRef,
+      points,
+    }
+  }
+});
 </script>
 
 <style scoped>
--- src/app.vue
+++ src/app.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="w-1/2">
     <j-count-down value="2021-07-2 00:00:00" />
+    <j-sparkline :data="[1,1,3,1]" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import JCountDown from '../libs/components/j-count-down.vue';
+import JSparkline from '../libs/components/j-sparkline.vue';
 
 export default defineComponent({
   name: 'App',
-  components: { JCountDown },
+  components: {JSparkline, JCountDown },
   data() {
     return {
       a: 102
